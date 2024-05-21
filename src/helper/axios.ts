9d512728b8a64b6cdcc2be33f8ce8e61ecd12200
RajThapa7 <jyangoraj123@gmail.com>
2024-05-21 12:22:33 +0545

    changes

--- src/helper/axios.ts
+++ src/helper/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://engineering-test-backend.onrender.com/",
+  baseURL: import.meta.env.VITE_BASE_URL,
 });
 
 api.interceptors.request.use(function (config) {
